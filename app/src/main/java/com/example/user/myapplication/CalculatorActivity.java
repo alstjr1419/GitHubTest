31cc7db3b11346e154b78c9db1e91a25df466a24
alstjr1419 <adsl1004>
2018-10-02 17:17:28 +0900

    calc implementing

--- app/src/main/java/com/example/user/myapplication/CalculatorActivity.java
+++ app/src/main/java/com/example/user/myapplication/CalculatorActivity.java
@@ -2,12 +2,93 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
     //constraint 공부하기
+    EditText firstInput,secondInput;
+    TextView operator, result;
+    TextView tv_calc_operator;
+    String firstText = "";
+    String secondText = "";
+    String opt = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        init();
+
+
     }
+
+    private void init(){
+        getSupportActionBar().setTitle("계산기");
+        firstInput = findViewById(R.id.firstInput);
+        firstInput.setShowSoftInputOnFocus(false);
+        secondInput = findViewById(R.id.secondInput);
+        secondInput.setShowSoftInputOnFocus(false);
+        operator = findViewById(R.id.operator);
+        result = findViewById(R.id.result);
+        tv_calc_operator = findViewById(R.id.operator);
+    }
+
+    public void clkRightBtn(View v){
+        if(v instanceof Button){
+            //강제형변환 및 버튼에 삽입
+            Button b = (Button)v;
+            //string 형변환
+            opt = b.getText().toString();
+            if(opt.equals("c")){
+                firstInput.setText("");
+                secondInput.setText("");
+                operator.setText("");
+                result.setText("");
+                firstText = "";
+                secondText = "";
+            }else{
+                tv_calc_operator.setText(opt);
+            }
+        }
+    }
+
+    public void clkLeftBtn(View v){
+        if(v instanceof Button){
+            Button b = (Button)v;
+            String num = b.getText().toString();
+            double res =0;
+
+            if(num.equals("=")){
+                if(firstText != "" && secondText != ""){
+                    int fir = Integer.parseInt(firstText);
+                    int sed = Integer.parseInt(secondText);
+                     if(opt.equals("+")){
+                         res = fir + sed;
+                     }else if(opt.equals("-")){
+                         res = fir - sed;
+                     }else if(opt.equals("*")){
+                         res = fir * sed;
+                     }else if(opt.equals("/")){
+                         res = (double)fir * (double)sed;
+                     }
+                     res.format
+                    String resu = Double.toString(res);
+                     result.setText(resu);
+                }
+            }
+
+            if(operator.getText().toString().equals("")){
+                firstText += num;
+                firstInput.setText(firstText);
+            }else{
+                secondText += num;
+                secondInput.setText(secondText);
+            }
+
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/user/myapplication/Ex11Activity.java
+++ app/src/main/java/com/example/user/myapplication/Ex11Activity.java
@@ -1,5 +1,6 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,25 +13,31 @@ public class Ex11Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex11);
-
+        showTitle();
     }
 
-    private void init(){
+    private void showTitle(){
         btn1 = findViewById(R.id.cal);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Ex11Activity.this, "버튼1", Toast.LENGTH_LONG).show();
+                Toast.makeText(Ex11Activity.this, "계산기", Toast.LENGTH_LONG).show();
+                //이동체 생성  ex11에서 calc클래스로 출발지와 목적지 설정(네비설정?).
+                Intent intent = new Intent(Ex11Activity.this, CalculatorActivity.class);
+                //startActivity메소드안에 intent객체를 넣어 서 보내면 이동한다. (택시?)
+                startActivity(intent);
             }
         });
         btn2 = findViewById(R.id.mp3);
-        btn2.setOnClickListener(new Button2());
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Ex11Activity.this, "mp3플레이어", Toast.LENGTH_LONG).show();
 
-    }
-    class Button2 implements View.OnClickListener{
-        @Override
-        public void onClick(View v){
-            Toast.makeText(Ex11Activity.this, "버튼2", Toast.LENGTH_LONG).show();
-        }
+                Intent intent = new Intent(Ex11Activity.this, Mp3playerActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
